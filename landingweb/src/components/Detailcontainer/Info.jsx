76cc3622da82a1eaa43fe752f31c4bcc5fc2096f
santosh <intelligencechannel86@gmail.com>
2021-12-30 09:05:58 +0545

    added all

---
+++ landingweb/src/components/Detailcontainer/Info.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Info.css"
+function Info() {
+    return (
+        <div className='info'>
+            <div className="info-item">
+                <img src="https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" />
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veniam mollitia quis quos perferendis autem amet non vitae quibusrum!</p>
+            </div>
+            <div className="info-item"></div>
+            
+        </div>
+    )
+}
+
+export default Info
---
+++ landingweb/src/components/Detailcontainer/InfoContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Info from "./Info";
+import "./Info.css"
+function InfoContainer() {
+  return <div className="infoContainer">
+      <Info/>
+      <Info/>
+  </div>;
+}
+
+export default InfoContainer;
